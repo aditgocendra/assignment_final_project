faf7cdda8556a4a9df3f0fcf6247dcb94a99acd1
aditgocendra <gocendra123@gmail.com>
2022-10-20 14:32:00 +0700

    add validator url link

--- models/photo_model.go
+++ models/photo_model.go
@@ -9,7 +9,7 @@ type Photo struct {
 	GormModel
 	Title    string `gorm:"not null" json:"title" form:"title" valid:"required~Title is required"`
 	Caption  string `gorm:"not null" json:"caption" form:"caption"`
-	PhotoUrl string `gorm:"not null" json:"photo_url" form:"photo_url" valid:"required~Photo url is required"`
+	PhotoUrl string `gorm:"not null" json:"photo_url" form:"photo_url" valid:"required~Photo url is required, url~Url format not valid"`
 	UserID   uint
 	User	 *User 	`json:",omitempty"`
 	Comments []Comment `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comments,omitempty"`
